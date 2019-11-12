9f5be9ad2066aed3448d4a1476e18dea408a4aff
ManituPC <chicherin.dn@gmail.com>
2019-11-12 23:47:19 +0200

    add labels

--- WeatherApp/MainScreen/MainViewController.swift
+++ WeatherApp/MainScreen/MainViewController.swift
@@ -29,5 +29,9 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 190.0
+    }
 }
 
