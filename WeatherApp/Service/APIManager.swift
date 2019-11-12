38bf1bd39164e08cf97bbca1b8ff13c0421ff046
ManituPC <chicherin.dn@gmail.com>
2019-11-12 23:14:03 +0200

    add models

---
+++ WeatherApp/Service/APIManager.swift
@@ -0,0 +1,14 @@
+//
+//  APIManager.swift
+//  WeatherApp
+//
+//  Created by denis.chicherin on 12.11.2019.
+//  Copyright © 2019 ChiAppCompany. All rights reserved.
+//
+
+import Foundation
+
+
+class APIManager {
+    
+}
